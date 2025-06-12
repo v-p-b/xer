26d253090c74e80ae1177fdccc9f13d7a42ac5f4
b <b@scrapco.de>
2025-06-12 12:20:56 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use nom::{
+  IResult,
+  Parser,
+  bytes::complete::{tag, take},
+};
+
+use std::num::ParseIntError;
+
+fn from_hex(input: &str) -> Result<u8, ParseIntError> {
+  u8::from_str_radix(input, 16)
+}
+
+fn hex_byte(input: &str) -> IResult<&str, u8> {
+    let (input, digits) = take(2usize)(input)?;
+    let res = from_hex(digits);
+    match res {
+        Ok(res) => return Ok((input,res)),
+        Err(_) => return Err(nom::Err::Error(nom::error::Error{input: input, code: nom::error::ErrorKind::HexDigit}))
+    }
+}
+
+fn hex_0x_byte(input: &str) -> IResult<&str, u8> {
+    let (input, _) = (tag("0x")).parse(input)?;
+    let (input, res) = hex_byte(input)?;
+    Ok((input, res))
+}
+
+fn main() {
+    assert_eq!(hex_0x_byte("0xab"), Ok(("", 0xab)));
+}
